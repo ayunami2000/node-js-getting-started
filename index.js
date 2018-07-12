486c6c280f4879b6263ca60b08c2b0264dc1d78f
ayunami2000 <spwilliamsiam@gmail.com>
2018-07-12 11:53:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-const { spawn } = require('child_process');
-const ls = spawn('java', ['-jar', 'server.jar']);
-
-ls.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
-});
-
-ls.stderr.on('data', (data) => {
-  console.log(`stderr: ${data}`);
-});
-
-ls.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
+const fs=require('fs');
+const https=require('https');
+const {spawn}=require('child_process');
+const server=spawn('java',['-jar','server.jar']);
+server.stdout.on('data',d=>console.log(d));
+server.stderr.on('data',d=>console.error(d));
+server.on('close',c=>console.log("Process closed with code "+c));
+https.get('https://yivesmirror.com/api/file/spigot/spigot-latest.jar',res=>{
+  let data='';
+  res.on('data',d=>data+=d.toString());
+  res.on('end',()=>{
+    if(fs.readFileSync('serverInfo.txt','utf8')==data)return console.log('Server is already up-to-date!');
+    fs.writeFileSync('serverInfo.txt',data);
+    https.get('https://yivesmirror.com/files/spigot/spigot-latest.jar',updata=>updata.pipe(fs.createWriteStream('server.jar')));
+  });
+}).on('error',e=>console.error(e));
